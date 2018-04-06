68b5a3583b55c8ac06e87af3bd8f79b9e7b90900
Joseph <joseph@Josephs-MacBook-Pro-2.local>
2018-04-06 00:16:38 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const hbs = require('hbs') //handlebars
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+app.use((req, res, next) => { //call on next to run the server.
+  res.render('maintanance.hbs', {
+    header: 'Maintanance'
+  })
+})
+
+hbs.registerHelper('getFullYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => { //with arguments
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to home page'
+  })
+
+  //API request
+  // res.send({
+  //   name: 'Joseph',
+  //   likes: [
+  //     'biking',
+  //     'cities'
+  //   ]
+  // })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Shit went wrong!!!'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+})
